8ea6c7859aa4b627aa84e9d876c64da635341a74
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-03-02 11:17:21 +0100

    form

--- src/main/java/classes/Agenda.java
+++ src/main/java/classes/Agenda.java
@@ -4,11 +4,11 @@ import javax.swing.*;
 
 public class Agenda {
     private JPanel panel1;
-    private JList list1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
+    private JList ContactList;
+    private JTextField MailText;
+    private JTextField Nametext;
+    private JTextField TelephoneText;
+    private JTextField SurnameText;
     private JButton AddButton;
     private JButton EditButton;
     private JButton ConsultButton;
